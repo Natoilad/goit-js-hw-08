c6fc2624f2ec8aca1ebb0ce7a7d6cfae95cf7035
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-14 01:42:04 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
+const conteinerGallary = document.querySelector(".gallery");
+const markupImg = createMarkupImg(galleryItems);
+conteinerGallary.insertAdjacentHTML("beforeend", markupImg);
+
+function createMarkupImg(item) {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"
+      </a>`,
+    ""
+  );
+  item.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  showCounter: false,
+});
 
 console.log(galleryItems);
