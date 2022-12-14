92089a155c34623a9c2629ba9ba9c338addc2cc4
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-14 02:29:20 +0200

    temp

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,10 @@
 import Player from "@vimeo/player";
+// import { throttle } from "lodash";
+
+const currentTime = "videoplayer - current - time";
+
+const iframe = document.querySelector("iframe");
+const iframePlayer = new Vimeo.Player(iframe);
 
 const player = new Player("handstick", {
   id: 19231868,
