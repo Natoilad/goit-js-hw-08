4aa15b2f4dbc28325ce997b5ccd7fc4b934054b6
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-18 00:40:38 +0200

    edit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("form");
+form.addEventListener("input", addToLocalStorege);
+const addToLocalStorege (e) {
 
+}
+form.addEventListener("submit", logData);
+
+const logData (e) {
+    e.preventDefault();
+    const mail = e.target.elements.input;
+    const message = e.target.elements.;
+    
+
+
+    console.dir({mail,message});
+}
