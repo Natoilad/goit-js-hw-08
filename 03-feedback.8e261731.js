5ba67103126398111312076503d32a6b4575762c
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-18 02:27:21 +0000

    Deploying to gh-pages from @ Natoilad/goit-js-hw-08@474462a6f4d02701ad743f620f09f884e90d7cee 🚀

--- 03-feedback.8e261731.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form");e.addEventListener("input",addToLocalStorege),e.addEventListener("submit",(function(e){e.preventDefault();const t=e.target.elements.input,n=e.target.elements.textarea;console.dir({mail:t,message:n})}));
-//# sourceMappingURL=03-feedback.8e261731.js.map
--- 03-feedback.af0659db.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("form");e.addEventListener("input",addToLocalStorege),e.addEventListener("submit",(function(e){e.preventDefault();var t=e.target.elements.input,n=e.target.elements.textarea;console.dir({mail:t,message:n})}))}();
-//# sourceMappingURL=03-feedback.af0659db.js.map
