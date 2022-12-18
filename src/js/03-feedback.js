474462a6f4d02701ad743f620f09f884e90d7cee
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-18 04:26:41 +0200

    task 3 comlete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,33 @@
-const form = document.querySelector("form");
-form.addEventListener("input", addToLocalStorege);
-// const addToLocalStorege (e) {
-
-// }
-form.addEventListener("submit", logData);
+import throttle from "lodash.throttle";
+const formE = document.querySelector("form");
+const keyData = "feedback-form-state";
+let localStorageData = {};
+formE.addEventListener("input", addToLocalStorege);
+function addToLocalStorege(e) {
+  localStorageData[e.target.name] = e.target.value;
+  localStorage.setItem(keyData, JSON.stringify(localStorageData));
+}
+formE.addEventListener("submit", logData);
 
 function logData(e) {
   e.preventDefault();
   const mail = e.target.elements.input;
-  const message = e.target.elements.textarea;
-
-  console.dir({ mail, message });
+  const message = e.target.message.value;
+  console.log({
+    mail,
+    message,
+  });
+  formE.reset();
+  localStorage.removeItem(keyData);
+}
+try {
+  const writeData = localStorage.getItem(keyData);
+  const parcData = JSON.parse(writeData);
+  if (writeData) {
+    formE.elements.email.value = parcData.email || "";
+    formE.elements.message.value = parcData.message || "";
+  }
+} catch (errorAlarm) {
+  console.log(errorAlarm.name);
+  console.log(errorAlarm.message);
 }
