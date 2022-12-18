dcc2b98b4441e1db4bbdf2e26d3be1918ffcc717
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-18 03:33:54 +0200

    task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,47 +1,19 @@
 import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
-// import Player from "@vimeo/player";
-// import throttle from "lodash.throttle";
 
 const iframe = document.querySelector("#vimeo-player");
 
-// const iframe = document.querySelector("#vimeo-plater");
-
 const player = new Player(iframe);
 
-const CURRENT_TIME = "videoplayer-current-time";
+const CURRENT_TARGET_TIME = "videoplayer-current-time";
 
-// const player = new Player(iframe);
+const previusTime = JSON.parse(localStorage.getItem(CURRENT_TARGET_TIME));
 
-// const CURRENT_TIME = "videoplayer-current-time";
-player.on("timeupdate", throttle(getTime, 1000));
-
-// player.on("timeupdate", throttle(timePlay, 1000));
-
-function getTime(data) {
+function targetTime(data) {
   const dataStringify = JSON.stringify(data.seconds);
-  localStorage.setItem(CURRENT_TIME, dataStringify);
+  localStorage.setItem(CURRENT_TARGET_TIME, dataStringify);
 }
 
-// function timePlay(data) {
-//   localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
-// }
-
-// const previusTime = JSON.parce(localStorage.getItem(CURRENT_TIME));
-
-const previusTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
-// const previusTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+player.on("timeupdate", throttle(targetTime, 1000));
 
 player.setCurrentTime(previusTime || 0);
-
-// player.setCurrentTime(previusTime || 0);
-
-// function continuePlay() {}
-// const player = new Player("handstick", {
-//   id: 19231868,
-//   width: 640,
-// });
-
-// player.on("play", function () {
-//   console.log("played the video!");
-// });
